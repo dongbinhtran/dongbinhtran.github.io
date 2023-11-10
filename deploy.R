968709907b99db2a5c6ec6cdcb3a0e6c9652181d
dongbinhtran <120134245+dongbinhtran@users.noreply.github.com>
2023-11-10 08:34:01 -0500

    Create deploy.R

---
+++ deploy.R
@@ -0,0 +1,14 @@
+
+blogdown::build_site()
+
+setwd('./public')
+
+rsconnect::deployApp(
+  ".",
+  appFiles = list.files(".", recursive = TRUE),
+  appPrimaryDoc = "index.html",
+  appTitle = "Stats Blog",
+  account = "steenogs",
+  server =  "rsc.pfizer.com",
+  appId = "a6dc2480-6b83-4e03-be94-34fdee6c4274"
+)
