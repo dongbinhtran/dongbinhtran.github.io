1d521c04c7cc746b6b29eeff78fc2676a845daa4
dongbinhtran <120134245+dongbinhtran@users.noreply.github.com>
2023-11-10 08:34:53 -0500

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -8,7 +8,7 @@ rsconnect::deployApp(
   appFiles = list.files(".", recursive = TRUE),
   appPrimaryDoc = "index.html",
   appTitle = "Stats Blog",
-  account = "steenogs",
+  account = "dongbinhtran",
   server =  "rsc.pfizer.com",
   appId = "a6dc2480-6b83-4e03-be94-34fdee6c4274"
 )
